6b63db05f4fc4ea6e9b8618e99e9cf3befb456d0
pavelnehaenko <pavel.ushlepkov@yandex.ru>
2023-05-29 20:02:12 +0300

    класс, чтобы добавлять новые чеки в базу данных и получать информацию о существующих через sqlite непонятно, переписал с pickle

--- data_base.py
+++ data_base.py
@@ -1,61 +1,47 @@
-import sqlite3
+import pickle
 
 class SalesDatabase:
-    def __init__(self, db_name='sales.db'):
-        self.db_name = db_name
-        self.connection = None
-
-    def connect(self):
-        # Подключение к базе данных
-        self.connection = sqlite3.connect(self.db_name)
-
-    def create_table(self):
-        # Создание таблицы для хранения информации о чеках
-        query = '''
-        CREATE TABLE IF NOT EXISTS sales (
-            id INTEGER PRIMARY KEY AUTOINCREMENT,
-            date TEXT,
-            dish TEXT,
-            price REAL
-        );
-        '''
-        self.connection.execute(query)
+    def __init__(self, db_file='sales_db.pkl'):
+        self.db_file = db_file
+        self.sales_data = []
 
     def add_sale(self, date, dish, price):
-        # Добавление информации о чеке в таблицу
-        query = '''
-        INSERT INTO sales (date, dish, price)
-        VALUES (?, ?, ?);
-        '''
-        self.connection.execute(query, (date, dish, price))
-        self.connection.commit()
+        # Добавление информации о чеке в базу данных
+        sale = {'date': date, 'dish': dish, 'price': price}
+        self.sales_data.append(sale)
+        self.save_data()
 
-    def get_sale_by_id(self, sale_id):
-        # Получение информации о чеке по его идентификатору
-        query = '''
-        SELECT * FROM sales WHERE id = ?;
-        '''
-        cursor = self.connection.execute(query, (sale_id,))
-        return cursor.fetchone()
+    def get_sale_by_index(self, index):
+        # Получение информации о чеке по его индексу в списке
+        if 0 <= index < len(self.sales_data):
+            return self.sales_data[index]
+        else:
+            return None
 
     def get_all_sales(self):
         # Получение информации о всех чеках
-        query = '''
-        SELECT * FROM sales;
-        '''
-        cursor = self.connection.execute(query)
-        return cursor.fetchall()
+        return self.sales_data
 
     def print_sale(self, sale):
         # Вывод информации о конкретном чеке
-        print(f"ID: {sale[0]}nDate: {sale[1]}nDish: {sale[2]}nPrice: {sale[3]}")
+        print(f"Date: {sale['date']}nDish: {sale['dish']}nPrice: {sale['price']}")
 
     def print_all_sales(self):
         # Вывод информации о всех чеках
-        sales = self.get_all_sales()
-        for sale in sales:
+        for i, sale in enumerate(self.sales_data):
+            print(f"Sale {i + 1}:")
             self.print_sale(sale)
+            print()
 
-    def close(self):
-        # Закрытие соединения с базой данных
-        self.connection.close()
+    def save_data(self):
+        # Сохранение данных в файл
+        with open(self.db_file, 'wb') as f:
+            pickle.dump(self.sales_data, f)
+
+    def load_data(self):
+        # Загрузка данных из файла
+        try:
+            with open(self.db_file, 'rb') as f:
+                self.sales_data = pickle.load(f)
+        except FileNotFoundError:
+            pass
