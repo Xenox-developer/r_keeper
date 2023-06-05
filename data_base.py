1fa54050fe22302514b02ec487178a3bbb63d357
pavelnehaenko <pavel.ushlepkov@yandex.ru>
2023-06-05 17:15:37 +0300

    вставил bill

--- data_base.py
+++ data_base.py
@@ -12,9 +12,10 @@ class Bill:
 
 
 class SalesDatabase:
-    def init(self, db_file: str = 'sales_db.pkl'):
+    def __init__(self, db_file: str):
         self.db_file = db_file
         self.sales_data = []
+        self.load_data()
 
     def add_bill(self, bill: Bill):
         self.sales_data.append(bill)
@@ -33,28 +34,32 @@ class SalesDatabase:
         # Получение информации о всех чеках
         return self.sales_data
 
-    def print_sale(self, sale: dict):
+    def print_sale(self, bill: Bill):
         # Вывод информации о конкретном чеке
-        if not isinstance(sale, dict):
-            raise TypeError('Sale must be a dictionary')
-        print(f"Date: {sale['date']}nDish: {sale['dish']}nPrice: {sale['price']}")
+        if not isinstance(bill, Bill):
+            raise TypeError('Bill must be an instance of the Bill class')
+        print(f"Date: {bill.date}nDishes: {bill.dishes_list}nTotal Sum: {bill.total_summ}")
 
     def print_all_sales(self):
         # Вывод информации о всех чеках
-        for i, sale in enumerate(self.sales_data):
+        for i, bill in enumerate(self.sales_data):
             print(f"Sale {i + 1}:")
-            self.print_sale(sale)
+            self.print_sale(bill)
             print()
 
     def save_data(self):
         # Сохранение данных в файл
-        with open(self.db_file, 'wb') as f:
-            pickle.dump(self.sales_data, f)
+        with open(self.db_file, 'w') as f:
+            for bill in self.sales_data:
+                f.write(f"{bill.Id}t{bill.date}t{bill.dishes_list}t{bill.total_summ}n")
 
     def load_data(self):
         # Загрузка данных из файла
         try:
-            with open(self.db_file, 'rb') as f:
-                self.sales_data = pickle.load(f)
+            with open(self.db_file, 'r') as f:
+                for line in f:
+                    data = line.strip().split('t')
+                    bill = Bill(int(data[0]), data[1], eval(data[2]), int(data[3]))
+                    self.sales_data.append(bill)
         except FileNotFoundError:
             pass
