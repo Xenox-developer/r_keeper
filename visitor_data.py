c096c8fc953d0e2d410fc16abe0493d438305f2c
Xenox-developer <Xenox1335@yandex.ru>
2023-06-06 18:04:10 +0300

    Клиент обрабатывается по номеру телефона

--- visitor_data.py
+++ visitor_data.py
@@ -1,95 +1,101 @@
 from dataclasses import dataclass
 
+
 @dataclass
 class Visitor:
-    id: int  # Идентификатор посетителя
-    first_name: str  # Имя посетителя
-    last_name: str  # Фамилия посетителя
-    contact_info: str  # Контактная информация посетителя
+    phone_number: str
+    first_name: str
+    last_name: str
 
 
 class VisitorDatabase:
     def __init__(self, db_file: str):
-        self.db_file = db_file  # Путь к файлу базы данных
-        self.visitors = []  # Список объектов Visitor для хранения информации о посетителях
-        self.loyalty_points = {}  # Словарь для хранения баллов лояльности посетителей
-        self.registration_info = {}  # Словарь для хранения информации о регистрации посетителей
-        self.discount_info = {}  # Словарь для хранения информации о предоставленных скидках
-        self.load_data()  # Загрузка данных из файла при инициализации объекта класса
-    
-    def add_visitor(self, visitor: Visitor):
-        self.visitors.append(visitor)  # Добавление нового посетителя в список
+        self.db_file = db_file
+        self.customers = []  # Список для хранения объектов Visitor
+        self.loyalty_points = {}  # Словарь для хранения баллов лояльности
+        self.registration_info = {}  # Словарь для хранения информации о регистрации
+        self.discount_info = {}  # Словарь для хранения информации о скидках
+        self.load_data()  # Загрузка данных из файла при создании экземпляра класса
 
-    def remove_visitor(self, visitor_id: int):
-        for visitor in self.visitors:
-            if visitor.id == visitor_id:
-                self.visitors.remove(visitor)  # Удаление посетителя из списка
-                break
-        if visitor_id in self.loyalty_points:
-            del self.loyalty_points[visitor_id]  # Удаление баллов лояльности посетителя, если они есть
-        if visitor_id in self.registration_info:
-            del self.registration_info[visitor_id]  # Удаление информации о регистрации посетителя, если она есть
-        if visitor_id in self.discount_info:
-            del self.discount_info[visitor_id]  # Удаление информации о предоставленных скидках посетителя, если она есть
+    def add_visitor(self, phone_number: str, first_name: str, last_name: str):
+        visitor = Visitor(phone_number, first_name, last_name)  # Создание объекта Visitor
+        self.customers.append(visitor)  # Добавление посетителя в список
+        self.save_data()  # Сохранение данных в файл
 
-    def get_visitor_info(self, visitor_id: int):
-        for visitor in self.visitors:
-            if visitor.id == visitor_id:
-                return visitor  # Возвращает информацию о посетителе по его идентификатору
+    def remove_visitor(self, phone_number: str):
+        # Удаление посетителя из списка на основе номера телефона
+        self.customers = [visitor for visitor in self.customers if visitor.phone_number != phone_number]
+        self.save_data()  # Сохранение данных в файл
+
+    def get_visitor_info(self, phone_number: str) -> Visitor:
+        # Получение информации о посетителе на основе номера телефона
+        for visitor in self.customers:
+            if visitor.phone_number == phone_number:
+                return visitor
         return None
 
-    def add_loyalty_points(self, visitor_id: int, points: int):
-        if visitor_id in self.loyalty_points:
-            self.loyalty_points[visitor_id] += points  # Увеличение количества баллов лояльности у посетителя, если он уже существует
+    def increase_loyalty_points(self, phone_number: str, points: int):
+        # Увеличение баллов лояльности у посетителя на основе номера телефона
+        if phone_number in self.loyalty_points:
+            self.loyalty_points[phone_number] += points
         else:
-            self.loyalty_points[visitor_id] = points  # Начисление баллов лояльности посетителю, если он новый
+            self.loyalty_points[phone_number] = points
+        self.save_data()  # Сохранение данных в файл
 
-    def use_loyalty_points(self, visitor_id: int, points: int):
-        if visitor_id in self.loyalty_points:
-            if self.loyalty_points[visitor_id] >= points:
-                self.loyalty_points[visitor_id] -= points  # Списание определенного количества баллов лояльности у посетителя для предоставления скидки
-                return True
+    def use_loyalty_points(self, phone_number: str, points: int) -> bool:
+        # Использование баллов лояльности для предоставления скидки
+        if phone_number in self.loyalty_points and self.loyalty_points[phone_number] >= points:
+            self.loyalty_points[phone_number] -= points
+            self.save_data()  # Сохранение данных в файл
+            return True
         return False
 
-    def add_discount_info(self, visitor_id: int, discount_info: dict):
-        self.discount_info[visitor_id] = discount_info  # Добавление информации о предоставленной скидке для конкретного посетителя
+    def add_discount(self, phone_number: str, discount_info: dict):
+        # Добавление информации о скидке для посетителя на основе номера телефона
+        self.discount_info[phone_number] = discount_info
+        self.save_data()  # Сохранение данных в файл
 
-    def check_discount_validity(self, visitor_id: int):
-        if visitor_id in self.discount_info:
-            # Здесь можно добавить логику для проверки действительности скидки
-            return True  # Проверка, действительна ли скидка для данного посетителя
-        return False
+    def check_discount_validity(self, phone_number: str) -> bool:
+        # Проверка действительности скидки для посетителя на основе номера телефона
+        return phone_number in self.discount_info
 
     def save_data(self):
         # Сохранение данных в файл
         with open(self.db_file, 'w') as f:
-            for visitor in self.visitors:
-                visitor_data = f"{visitor.id}\t{visitor.first_name}\t{visitor.last_name}\t{visitor.contact_info}\n"
-                f.write(visitor_data)  # Запись информации о посетителях в файл, разделяя атрибуты символом табуляции
+            f.write("Customers:\n")
+            for visitor in self.customers:
+                f.write(f"{visitor.phone_number},{visitor.first_name},{visitor.last_name}\n")
+            f.write("Loyalty Points:\n")
+            for phone_number, points in self.loyalty_points.items():
+                f.write(f"{phone_number},{points}\n")
+            f.write("Discount Info:\n")
+            for phone_number, discount_info in self.discount_info.items():
+                f.write(f"{phone_number},{discount_info}\n")
 
     def load_data(self):
         # Загрузка данных из файла
-        try:
-            with open(self.db_file, 'r') as f:
-                for line in f:
-                    data = line.strip().split('\t')
-                    visitor = Visitor(int(data[0]), data[1], data[2], data[3])
-                    self.visitors.append(visitor)  # Чтение данных из файла и создание объектов Visitor для каждого посетителя
-        except FileNotFoundError:
-            pass  # Если файл не найден, пропускаем загрузку данных
-
-    def get_loyalty_points_by_name(self, first_name: str, last_name: str):
-        for visitor in self.visitors:
-            if visitor.first_name == first_name and visitor.last_name == last_name:
-                visitor_id = visitor.id
-                if visitor_id in self.loyalty_points:
-                    return self.loyalty_points[visitor_id]
-        return -1  # Возвращает -1, если посетитель с указанным именем и фамилией не найден
-
-    def add_loyalty_points_by_name(self, first_name: str, last_name: str, points: int):
-        for visitor in self.visitors:
-            if visitor.first_name == first_name and visitor.last_name == last_name:
-                visitor_id = visitor.id
-                self.add_loyalty_points(visitor_id, points)
-                return True
-        return False  # Возвращает False, если посетитель с указанным именем и фамилией не найден
+        self.customers = []  # Очистка списка посетителей
+        self.loyalty_points = {}  # Очистка словаря баллов лояльности
+        self.discount_info = {}  # Очистка словаря информации о скидках
+        with open(self.db_file, 'r') as f:
+            current_section = None  # Переменная для хранения текущего раздела данных
+            for line in f:
+                line = line.strip()
+                if line == "Customers:":
+                    current_section = "Customers"  # Установка текущего раздела данных
+                elif line == "Loyalty Points:":
+                    current_section = "Loyalty Points"  # Установка текущего раздела данных
+                elif line == "Discount Info:":
+                    current_section = "Discount Info"  # Установка текущего раздела данных
+                else:
+                    if current_section == "Customers":
+                        # Разбивка строки на номер телефона, имя и фамилию посетителя
+                        phone_number, first_name, last_name = line.split(",")
+                        visitor = Visitor(phone_number, first_name, last_name)  # Создание объекта Visitor
+                        self.customers.append(visitor)  # Добавление посетителя в список
+                    elif current_section == "Loyalty Points":
+                        phone_number, points = line.split(",")
+                        self.loyalty_points[phone_number] = int(points)  # Добавление баллов лояльности в словарь
+                    elif current_section == "Discount Info":
+                        phone_number, discount_info = line.split(",", 1)
+                        self.discount_info[phone_number] = discount_info  # Добавление информации о скидке в словарь
